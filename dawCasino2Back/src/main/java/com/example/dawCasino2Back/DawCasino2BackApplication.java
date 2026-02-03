a3d0a4402ff104cd75a2c37f7b43b2afc72bc78c
Mario Ascaso <ascasovicentemario@gmail.com>
2026-02-03 18:26:31 +0100

    first commit

---
+++ dawCasino2Back/src/main/java/com/example/dawCasino2Back/DawCasino2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dawCasino2Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawCasino2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawCasino2BackApplication.class, args);
+	}
+
+}
